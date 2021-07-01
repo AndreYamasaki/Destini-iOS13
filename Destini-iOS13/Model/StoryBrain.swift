4b561ecd21d86109ccdc1d63c63d70905d840095
Andre <andreyamasaki@brq.com>
2021-07-01 12:07:19 -0300

    step 5 complete

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,33 @@
 
 import Foundation
 
-
+struct StoryBrain {
+    
+    let listOfStory = [
+    Story(title: "You see a for on the road", choice1: "You take a left", choice1Destination: 1, choice2: "Take a right", choice2Destination: 2),
+        Story(title: "You see a tiger", choice1: "Shout for help", choice1Destination: 1, choice2: "Play Dead", choice2Destination: 2),
+        Story(title: "You see a treasure chest", choice1: "Open it", choice1Destination: 1, choice2: "Look for traps", choice2Destination: 2)
+    ]
+    
+    var storyNumber = 0
+    
+    func getStoryText() -> String {
+        return listOfStory[storyNumber].title
+    }
+    
+    func getChoice1() -> String {
+        return listOfStory[storyNumber].choice1
+    }
+    
+    func getChoice2() -> String {
+        return listOfStory[storyNumber].choice2
+    }
+    
+    mutating func nextStory(userChoice: String) {
+        if userChoice == listOfStory[storyNumber].choice1 {
+            storyNumber = listOfStory[storyNumber].choice1Destination
+        } else {
+            storyNumber = listOfStory[storyNumber].choice2Destination
+        }
+    }
+}
